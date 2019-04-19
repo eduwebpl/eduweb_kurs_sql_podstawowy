5f4ca0f4dadeac474a5e0397381210efb3d8042c
Mikołaj Antoszkiewicz <mikolaj.antoszkiewicz@gmail.com>
2019-04-19 14:03:15 +0200

    02.1-finish

---
+++ 02/01-dodanie_nowej_kolumny_NOT_NULL.sql
@@ -0,0 +1,29 @@
+-- Kolumna mająca ograniczenie NOT NULL nie może mieć pustych pól
+-- Dodanie nowej kolumny NOT NULL używając domyślnych wartości (DEFAULT) do wypełnienia jej danymi
+
+ALTER TABLE heroes ADD COLUMN is_teamleader boolean DEFAULT False NOT NULL;
+
+-- Sprawdzenie wstawionych wartości do nowej kolumny
+SELECT is_teamleader, hero_id FROM heroes;
+
+-- Usunięcie nowej kolumny
+ALTER TABLE heroes DROP COLUMN is teamleader;
+
+-- Dodanie nowej kolumny (bez ograniczenia NOT NULL)
+ALTER TABLE heroes ADD COLUMN is_teamleader boolean;
+
+-- Sprawdzenie wartości w nowej kolumnie
+SELECT is_teamleader, hero_id FROM heroes;
+
+-- Wypełnienie kolumny losowymi wartościami
+-- (Potrzebne rzutowanie żeby z wartości ułamkowej z funkcji RANDOM() otrzymać wartość logiczną)
+UPDATE heroes SET is_teamleader = CAST(CAST(ROUND(RANDOM()) AS int) AS boolean);
+
+-- Ustawienie ograniczenia NOT NULL na nowej kolumnie
+ALTER TABLE heroes ALTER COLUMN is_teamleader SET NOT NULL;
+
+-- Próba ustawienia wartości pustej (NULL) w nowej kolumnie (powinna się nie powieść i spowodować błąd)
+UPDATE heroes SET is_teamleader = NULL;
+
+-- Dodanie nowej kolumny
+ALTER TABLE heroes ADD COLUMN gold int DEFAULT 0 NOT NULL;
