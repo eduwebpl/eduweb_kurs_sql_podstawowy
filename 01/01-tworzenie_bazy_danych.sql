0e6b67ed782a01dc6dca518d6b5b902970621cc6
Mikołaj Antoszkiewicz <mikolaj.antoszkiewicz@gmail.com>
2019-04-19 10:07:20 +0200

    01-finish

---
+++ 01/01-tworzenie_bazy_danych.sql
@@ -0,0 +1,6 @@
+-- To jest linia komentarza kodu SQL
+
+-- Tworzenie bazy danych
+-- (To zapytanie powinno zosta? wykonane na pocz?tku zanim zostanie wykonane nast?pne)
+
+CREATE DATABASE sqlandia;
---
+++ 01/02-zmiana_nazwy_bazy_danych.sql
@@ -0,0 +1,4 @@
+-- Zmiana nazywy bazy
+-- (To zapytanie powinno by? wykonane po stworzeniu bazy 'sqlandia')
+
+ALTER DATABASE sqlandia RENAME TO sqland;
