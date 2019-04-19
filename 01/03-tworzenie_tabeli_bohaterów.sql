d28f588da1a7619895c5ebee0c4a1901ed1a4bb7
Mikołaj Antoszkiewicz <mikolaj.antoszkiewicz@gmail.com>
2019-04-19 10:31:45 +0200

    01.2-finish

---
+++ 01/03-tworzenie_tabeli_bohaterów.sql
@@ -0,0 +1,25 @@
+-- Poni?sze zapytania powinny zosta? wykonane po po??czeniu si? z nowo powsta?? baz? 'sqland'
+
+-- Tworzenie pustej tabeli
+CREATE TABLE heroes ();
+
+-- Usuwanie tabeli
+DROP TABLE heroes;
+
+-- Tworzenie normalnej tabeli z kolumnami
+CREATE TABLE heroes (
+    first_name varchar,
+    last_name varchar,
+    gender char[1],
+    race varchar,
+    strength int,
+    wisdom int,
+    health int,
+    mana int
+);
+
+-- Dodanie kolumny identyfikatora wiersza
+ALTER TABLE heroes ADD COLUMN hero_id int;
+
+-- Poprawa kolumny gender -> zmiana typu z jednoelementowej listy na pojedynczy znak
+ALTER TABLE heroes ALTER COLUMN gender TYPE char(1);
