1774e2e31e1d34d69231c3742542169650c7b4f7
Mikołaj Antoszkiewicz <mikolaj.antoszkiewicz@gmail.com>
2019-04-19 18:29:36 +0200

    02.2-finish

---
+++ 02/Dane do lekcji 2 rozdziału 2 - pod zadania.sql
@@ -0,0 +1,6 @@
+INSERT INTO heroes (first_name, last_name, gender, race, strength, wisdom, health, mana, hero_id, birthdate) 
+	VALUES ('Johnny', 'Monotonic', 'M', 'człowiek', 23, 33, 250, 390, 10, '1995-11-17');
+INSERT INTO heroes (first_name, last_name, gender, race, strength, wisdom, health, mana, hero_id, birthdate) 
+	VALUES ('Axel', 'Pulley', 'M', 'człowiek', 19, 21, 170, 230, 11, '1984-12-01');
+INSERT INTO heroes (first_name, last_name, gender, race, strength, wisdom, health, mana, hero_id, birthdate) 
+	VALUES ('Xena', 'Flawless', 'K', 'człowiek', 31, 10, 320, 90, 12, '1995-09-04');
